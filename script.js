bc029a36db364b265a4f4fac5cf8d63fea1ff5b7
Cristian Petrila <cristianpetrila@icloud.com>
2024-07-17 13:59:31 +0100

    tasksss

---
+++ script.js
@@ -0,0 +1,17 @@
+function openModal(imageSrc) {
+    const modal = document.getElementById('imageModal');
+    const modalImg = document.getElementById('modalImage');
+    modal.style.display = "flex";
+    modalImg.src = imageSrc;
+}
+
+function closeModal() {
+    document.getElementById('imageModal').style.display = "none";
+}
+
+window.onclick = function(event) {
+    const modal = document.getElementById('imageModal');
+    if (event.target == modal) {
+        closeModal();
+    }
+}
